5128ae9ef6710b296a45a117b839c1722a1c6572
sams3pi0l <sams3pi0l@sams3pi0l.sams3pi0l>
2025-08-10 19:26:08 +0530

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -119,16 +119,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_URL = '/login/' 
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'recipe/static/'),
-)
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'recipe/static/')
 
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'recipe/static/'),  # source files
+)
+
+if not DEBUG:
+    # Where collected static files will go
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # WhiteNoise for compressed static file serving
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
